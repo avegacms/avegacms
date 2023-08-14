02ca271c8c30693af56050b96521c3f82046a14e
AvegaCMS <web@avega-soft.ru>
2023-08-14 18:01:07 +0600

    Создание функционала Locales

--- src/Controllers/Api/Admin/Settings/Locales.php
+++ src/Controllers/Api/Admin/Settings/Locales.php
@@ -3,8 +3,21 @@
 namespace AvegaCms\Controllers\Api\Admin\Settings;
 
 use AvegaCms\Controllers\Api\AvegaCmsAPI;
+use AvegaCms\Models\Admin\LocalesModel;
 
 class Locales extends AvegaCmsAPI
 {
+    protected LocalesModel $LM;
 
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->LM = model(LocalesModel::class);
+    }
+
+    public function index()
+    {
+        return $this->respond(['data' => 'Locales/index']);
+    }
 }
