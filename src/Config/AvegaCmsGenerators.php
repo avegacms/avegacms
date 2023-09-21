440e205eb5ac1863b0f86e1c5d4e110f31dfa57e
AvegaCMS <web@avega-soft.ru>
2023-09-21 12:59:07 +0600

    Создание AvegaCmsScaffoldGenerator.php

---
+++ src/Config/AvegaCmsGenerators.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Config;
+
+class AvegaCmsGenerators
+{
+    public static $views = [
+        'avegacms:controller' => 'AvegaCms\Commands\Generators\Views\avegacmscontroller.tpl.php',
+        'avegacms:model'      => 'AvegaCms\Commands\Generators\Views\avegacmsmodel.tpl.php',
+    ];
+}
