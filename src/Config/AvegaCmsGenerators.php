c796484aea9964679e2976f4f3e53d63aa157d6e
AvegaCMS <web@avega-soft.ru>
2023-09-23 15:10:31 +0600

    Добавлено Migrator.php

--- src/Config/AvegaCmsGenerators.php
+++ src/Config/AvegaCmsGenerators.php
@@ -7,5 +7,6 @@ class AvegaCmsGenerators
     public static $views = [
         'avegacms:controller' => 'AvegaCms\Commands\Generators\Views\avegacmscontroller.tpl.php',
         'avegacms:model'      => 'AvegaCms\Commands\Generators\Views\avegacmsmodel.tpl.php',
+        'avegacms:migration'  => 'AvegaCms\Commands\Generators\Views\avegacmsmigration.tpl.php'
     ];
 }
