9882833b409a3dea12038c844aa76e696522163c
AvegaCMS <web@avega-soft.ru>
2023-10-24 17:17:48 +0600

    Правка MetaDataModel/getSubPages

---
+++ src/Enums/FieldsTypes.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AvegaCms\Enums;
+
+enum FieldsTypes: string
+{
+    case Button   = 'button';
+    case Checkbox = 'checkbox';
+    case Color    = 'color';
+    case Date     = 'date';
+    case Email    = 'email';
+    case File     = 'file';
+    case Hidden   = 'hidden';
+    case Image    = 'image';
+    case Number   = 'number';
+    case Password = 'password';
+    case Radio    = 'radio';
+    case Range    = 'range';
+    case Reset    = 'reset';
+    case Search   = 'search';
+    case Submit   = 'submit';
+    case Tel      = 'tel';
+    case Text     = 'text';
+    case Textarea = 'textarea';
+    case Time     = 'time';
+    case Url      = 'url';
+    case Week     = 'week';
+}
