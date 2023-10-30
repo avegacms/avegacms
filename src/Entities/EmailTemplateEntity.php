bfdef9f96d01a9d3f63b3d4935701ff2c2e77701
AvegaCMS <web@avega-soft.ru>
2023-10-30 17:08:20 +0600

    Добавление @property

--- src/Entities/EmailTemplateEntity.php
+++ src/Entities/EmailTemplateEntity.php
@@ -4,9 +4,28 @@ namespace AvegaCms\Entities;
 
 use CodeIgniter\Entity\Entity;
 
+/**
+ * @property int|null $id
+ * @property string|null $label
+ * @property string|null $slug
+ * @property int|null $localeId
+ * @property int|null $isSystem
+ * @property string|null $subject
+ * @property string|null $content
+ * @property string|null $variables
+ * @property string|null $template
+ * @property int|null $active
+ * @property int|null $createdById
+ * @property int|null $updatedById
+ */
 class EmailTemplateEntity extends Entity
 {
-    protected $datamap = [];
+    protected $datamap = [
+        'localeId'    => 'locale_id',
+        'isSystem'    => 'is_system',
+        'createdById' => 'created_by_id',
+        'updatedById' => 'updated_by_id'
+    ];
     protected $dates   = ['created_at', 'updated_at'];
     protected $casts   = [
         'id'            => 'integer',
