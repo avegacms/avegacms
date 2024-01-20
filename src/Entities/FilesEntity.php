930075623d0dd3ed206c16cda6a3f238a52a4c15
Avega <web@avega-soft.ru>
2024-01-20 13:54:28 +0600

    [RGRNP-166] :: Создание мультидульного файлового менеджера

--- src/Entities/FilesEntity.php
+++ src/Entities/FilesEntity.php
@@ -5,59 +5,33 @@ namespace AvegaCms\Entities;
 use CodeIgniter\Entity\Entity;
 
 /**
- * @property int|null $id
- * @property int|null $userId
- * @property string|null $name
- * @property string|null $alternativeText
- * @property string|null $caption
- * @property int|null $width
- * @property int|null $height
- * @property string|null $formats
- * @property string|null $hash
- * @property string|null $ext
- * @property float|null $size
- * @property string|null $url
- * @property string|null $previewUrl
- * @property string|null $provider
- * @property string|null $providerMetaData
- * @property string|null $folderPath
- * @property int|null $isPersonal
- * @property string|null $fileType
+ * @property int $id
+ * @property int $providerId
+ * @property string $provider
+ * @property string $data
+ * @property string $type
+ * @property boolean $active
+ * @property int $createdById
+ * @property int $updatedById
  */
 class FilesEntity extends Entity
 {
     protected $datamap = [
-        'userId'           => 'user_id',
-        'alternativeText'  => 'alternative_text',
-        'previewUrl'       => 'preview_url',
-        'providerMetaData' => 'provider_metadata',
-        'folderPath'       => 'folder_path',
-        'isPersonal'       => 'is_personal',
-        'fileType'         => 'file_type'
+        'providerId'  => 'provider_id',
+        'createdById' => 'created_by_id',
+        'updatedById' => 'updated_by_id',
     ];
     protected $dates   = ['created_at', 'updated_at'];
     protected $casts   = [
-        'id'                => 'integer',
-        'user_id'           => 'integer',
-        'name'              => 'string',
-        'alternative_text'  => 'string',
-        'caption'           => 'string',
-        'width'             => 'integer',
-        'height'            => 'integer',
-        'formats'           => 'json-array',
-        'hash'              => 'string',
-        'ext'               => 'string',
-        'size'              => 'float',
-        'url'               => 'string',
-        'preview_url'       => 'string',
-        'provider'          => 'string',
-        'provider_metadata' => 'json-array',
-        'folder_path'       => 'string',
-        'is_personal'       => 'integer',
-        'file_type'         => 'string',
-        'created_by_id'     => 'integer',
-        'updated_by_id'     => 'integer',
-        'created_at'        => 'datetime',
-        'updated_at'        => 'datetime',
+        'id'            => 'integer',
+        'provider_id'   => 'integer',
+        'provider'      => 'string',
+        'data'          => 'json-array',
+        'type'          => 'string',
+        'active'        => 'int-bool',
+        'created_by_id' => 'integer',
+        'updated_by_id' => 'integer',
+        'created_at'    => 'datetime',
+        'updated_at'    => 'datetime',
     ];
 }
---
+++ src/Models/Admin/FilesLinksModel.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace AvegaCms\Models\Admin;
+
+use AvegaCms\Models\AvegaCmsModel;
+use AvegaCms\Entities\FilesLinksEntity;
+
+class FilesLinksModel extends AvegaCmsModel
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'files_link';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = false;
+    protected $returnType       = FilesLinksEntity::class;
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'id',
+        'user_id',
+        'parent',
+        'module_id',
+        'entity_id',
+        'item_id',
+        'uid',
+        'created_at',
+        'updated_at'
+    ];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [
+        'id'        => ['rules' => 'if_exist|is_natural'],
+        'user_id'   => ['rules' => 'if_exist|is_natural_no_zero'],
+        'parent'    => ['rules' => 'if_exist|is_natural'],
+        'module_id' => ['rules' => 'if_exist|is_natural'],
+        'entity_id' => ['rules' => 'if_exist|is_natural'],
+        'item_id'   => ['rules' => 'if_exist|is_natural'],
+        'uid'       => ['rules' => 'if_exist|permit_empty|max_length[64]'],
+    ];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+
+    // AvegaCms filter settings
+    protected array  $filterFields      = [];
+    protected array  $searchFields      = [];
+    protected array  $sortableFields    = [];
+    protected array  $filterCastsFields = [];
+    protected string $searchFieldAlias  = 'q';
+    protected string $sortFieldAlias    = 's';
+    protected string $sortDefaultFields = '';
+    protected array  $filterEnumValues  = [];
+    protected int    $limit             = 20;
+    protected int    $maxLimit          = 100;
+}
