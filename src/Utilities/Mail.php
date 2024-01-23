5f7cd40a42f41cff53cd9761bdc3c829054e183c
Avega <web@avega-soft.ru>
2024-01-23 14:44:09 +0600

    [RGRNP-35] :: Создание email шаблона

--- src/Utilities/Mail.php
+++ src/Utilities/Mail.php
@@ -95,6 +95,12 @@ class Mail
             $email->setReplyTo($config['replyEmail'], $config['replyName'] ?? '');
         }
 
+        $emailData['foundation'] = [
+            ...$emailData['foundation'] ?? [],
+            'autoSentEmail'      => $config['autoSentEmail'],
+            'cancelSubscription' => $config['cancelSubscription']
+        ];
+
         $email->setMessage(
             ($config['mailType'] === 'html') ? view('template/email/foundation', $emailData,
                 ['debug' => false]) : $emailData['content']
@@ -155,27 +161,31 @@ class Mail
 
         return [
 
-            'fromEmail'     => $config['fromEmail'] ?? $defConfig['fromEmail'],
-            'fromName'      => $config['fromName'] ?? $defConfig['fromEmail'],
-            'replyEmail'    => $config['replyEmail'] ?? $defConfig['replyEmail'],
-            'replyName'     => $config['replyName'] ?? $defConfig['replyName'],
-            'returnEmail'   => $config['returnEmail'] ?? $defConfig['returnEmail'],
-            'userAgent'     => $config['userAgent'] ?? $defConfig['userAgent'],
-            'protocol'      => strtolower(($config['protocol'] ?? $defConfig['protocol']) ?? 'mail'),
-            'wordWrap'      => $config['wordWrap'] ?? 76,
-            'validate'      => true,
-            'mailType'      => strtolower($config['mailType'] ?? $defConfig['mailType']),
-            'charset'       => $config['charset'] ?? $defConfig['charset'],
-            'priority'      => $config['priority'] ?? $defConfig['priority'],
+            'fromEmail'          => $config['fromEmail'] ?? $defConfig['fromEmail'],
+            'fromName'           => $config['fromName'] ?? $defConfig['fromEmail'],
+            'replyEmail'         => $config['replyEmail'] ?? $defConfig['replyEmail'],
+            'replyName'          => $config['replyName'] ?? $defConfig['replyName'],
+            'returnEmail'        => $config['returnEmail'] ?? $defConfig['returnEmail'],
+            'userAgent'          => $config['userAgent'] ?? $defConfig['userAgent'],
+            'protocol'           => strtolower(($config['protocol'] ?? $defConfig['protocol']) ?? 'mail'),
+            'wordWrap'           => $config['wordWrap'] ?? 76,
+            'validate'           => true,
+            'mailType'           => strtolower($config['mailType'] ?? $defConfig['mailType']),
+            'charset'            => $config['charset'] ?? $defConfig['charset'],
+            'priority'           => $config['priority'] ?? $defConfig['priority'],
 
-            // SMTP - насройки
-            'smtpHost'      => $config['smtpHost'] ?? $defConfig['smtpHost'],
-            'smtpUser'      => $config['smtpUser'] ?? $defConfig['smtpUser'],
-            'smtpPass'      => $config['smtpPass'] ?? $defConfig['smtpPass'],
-            'smtpPort'      => $config['smtpPort'] ?? $defConfig['smtpPort'],
-            'smtpTimeout'   => $config['smtpTimeout'] ?? $defConfig['smtpTimeout'],
-            'smtpKeepalive' => $config['smtpKeepalive'] ?? $defConfig['smtpKeepalive'],
-            'smtpCrypto'    => $config['smtpCrypto'] ?? $defConfig['smtpCrypto']
+            // SMTP - настройки
+            'smtpHost'           => $config['smtpHost'] ?? $defConfig['smtpHost'],
+            'smtpUser'           => $config['smtpUser'] ?? $defConfig['smtpUser'],
+            'smtpPass'           => $config['smtpPass'] ?? $defConfig['smtpPass'],
+            'smtpPort'           => $config['smtpPort'] ?? $defConfig['smtpPort'],
+            'smtpTimeout'        => $config['smtpTimeout'] ?? $defConfig['smtpTimeout'],
+            'smtpKeepalive'      => $config['smtpKeepalive'] ?? $defConfig['smtpKeepalive'],
+            'smtpCrypto'         => $config['smtpCrypto'] ?? $defConfig['smtpCrypto'],
+
+            // Доп. настройки
+            'autoSentEmail'      => $config['autoSentEmail'] ?? true,
+            'cancelSubscription' => $config['cancelSubscription'] ?? false,
         ];
     }
 }
