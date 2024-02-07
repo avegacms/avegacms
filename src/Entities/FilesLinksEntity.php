3029780c7aef127ab9e6b3acd6e5db8fce304b82
Avega <web@avega-soft.ru>
2024-02-07 11:56:13 +0600

    [RGRNP-166] :: Создание мультимодульного файлового менеджера

--- src/Entities/FilesLinksEntity.php
+++ src/Entities/FilesLinksEntity.php
@@ -43,7 +43,6 @@ class FilesLinksEntity extends AvegaCmsEntity
         'created_at'    => 'datetime',
         'updated_at'    => 'datetime',
 
-        'data'     => 'json-array',
         'provider' => 'integer',
     ];
 
