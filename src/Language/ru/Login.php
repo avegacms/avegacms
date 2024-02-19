f57eebd643618710367799d5b62226665fcebd58
Avega <web@avega-soft.ru>
2024-02-19 14:49:25 +0600

    Добавлен список статусов пользователей

---
+++ src/Language/ru/Login.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'fields' => [
+        'login'     => 'Логин',
+        'token'     => 'Токен',
+        'email'     => 'Email',
+        'phone'     => 'Телефон',
+        'password'  => 'Пароль',
+        'code'      => 'Код',
+        'condition' => 'Условие'
+    ],
+    'errors' => [
+        'isNotUnique'        => 'Пользователь не найден в системе',
+        'unknownUser'        => 'Пользователь не найден в системе',
+        'wrongPassword'      => 'Неправильный пароль',
+        'wrongToken'         => 'Неправильный токен',
+        'codeExpired'        => 'Время действия кода истекло',
+        'wrongCode'          => 'Неверный код',
+        'createToken'        => 'Ошибка создания токена',
+        'expiresToken'       => 'Время действия токена истекло',
+        'tokenNotFound'      => 'Токен не обнаружен',
+        'failedSendAuthCode' => 'Не удалось отправить код авторизации',
+    ]
+];
+
---
+++ src/Language/ru/Users.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'enums'  => [
+        'Registration' => 'Регистрация',
+        'Active'       => 'Активно',
+        'Moderated'    => 'На модерации',
+        'Banned'       => 'Заблокирован',
+        'Deleted'      => 'Диактивирован',
+        'NotDefined'   => 'Неопределён',
+    ],
+    'fields' => [
+    ],
+    'errors' => [
+    ]
+];
