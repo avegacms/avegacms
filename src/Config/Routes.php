ec7355dcaaacbfe6d2494c972f5cc0603d1d9f14
Avega <web@avega-soft.ru>
2024-02-26 20:28:59 +0600

    Доработка CORS

--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -19,7 +19,8 @@ use AvegaCms\Controllers\Api\Admin\Settings\{Locales,
  * @var RouteCollection $routes
  */
 
-$routes->options('(:any)', [Cors::class, 'PreFlight']);
+$routes->options('(:any)', '', ['filter' => 'cors']);
+//$routes->options('(:any)', [Cors::class, 'PreFlight']);
 
 $routes->group('api', function (RouteCollection $routes) {
     $routes->group('public', ['namespace' => 'AvegaCms\Controllers\Api\Public'], function (RouteCollection $routes) {
