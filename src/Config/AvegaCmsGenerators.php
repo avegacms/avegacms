4c8629e6e2c2138811fc1c9d2f7346aefe40a43b
Avega <web@avega-soft.ru>
2024-03-21 00:13:29 +0600

    Добавление в команду avegacms:module Factories

--- src/Config/AvegaCmsGenerators.php
+++ src/Config/AvegaCmsGenerators.php
@@ -6,6 +6,7 @@ class AvegaCmsGenerators
 {
     public static array $views = [
         'avegacms:controller' => 'AvegaCms\Commands\Generators\Views\avegacmscontroller.tpl.php',
+        'avegacms:factory'    => 'AvegaCms\Commands\Generators\Views\avegacmsfactory.tpl.php',
         'avegacms:model'      => 'AvegaCms\Commands\Generators\Views\avegacmsmodel.tpl.php',
         'avegacms:migration'  => 'AvegaCms\Commands\Generators\Views\avegacmsmigration.tpl.php',
         'avegacms:sitemap'    => 'AvegaCms\Commands\Generators\Views\avegacmssitemapcontroller.tpl.php',
