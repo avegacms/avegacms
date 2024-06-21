1e987f426346f99cd518ef522a78be1ef1585fd6
Avega <web@avega-soft.ru>
2024-06-21 21:59:34 +0600

    Добавлены AvegaCmsApiException в AvegaCmsAPI

--- src/Exceptions/AvegaCmsApiException.php
+++ src/Exceptions/AvegaCmsApiException.php
@@ -11,23 +11,24 @@ class AvegaCmsApiException extends Exception
     /**
      * @return AvegaCmsApiException
      */
-    public function fonNoData(): AvegaCmsApiException
+    public static function forNoData(): AvegaCmsApiException
     {
         return new static(message: lang('Api.errors.noData'));
     }
 
     /**
+     * @param  string|null  $error
      * @return AvegaCmsApiException
      */
-    public function forInvalidJSON(): AvegaCmsApiException
+    public static function forInvalidJSON(string $error = null): AvegaCmsApiException
     {
-        return new static(message: lang('Api.errors.invalidJSON'));
+        return new static(message: lang('Api.errors.invalidJSON', [$error]));
     }
 
     /**
      * @return AvegaCmsApiException
      */
-    public function forUndefinedData(): AvegaCmsApiException
+    public static function forUndefinedData(): AvegaCmsApiException
     {
         return new static(message: lang('Api.errors.undefinedData'));
     }
