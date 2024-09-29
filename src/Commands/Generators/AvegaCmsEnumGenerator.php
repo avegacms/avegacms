6fd1cafae3eddd2d23cddba9dfa749f6c312741e
Boss <web@avega-soft.ru>
2024-09-29 11:35:16 +0600

    Добавление SPARK команду создания Enum

---
+++ src/Commands/Generators/AvegaCmsEnumGenerator.php
@@ -0,0 +1,73 @@
+<?php
+declare(strict_types=1);
+namespace AvegaCms\Commands\Generators;
+
+use AvegaCms\Config\AvegaCms;
+use CodeIgniter\CLI\BaseCommand;
+use CodeIgniter\CLI\GeneratorTrait;
+
+class AvegaCmsEnumGenerator extends BaseCommand
+{
+    use GeneratorTrait;
+    /**
+     * The Command's Group
+     *
+     * @var string
+     */
+    protected $group = 'AvegaCMS (v ' . AvegaCms::AVEGACMS_VERSION . ')';
+
+    /**
+     * The Command's Name
+     *
+     * @var string
+     */
+    protected $name = 'avegacms:enum';
+
+    /**
+     * The Command's Description
+     *
+     * @var string
+     */
+    protected $description = 'Generates a new Enum file extended from.';
+
+    /**
+     * The Command's Usage
+     *
+     * @var string
+     */
+    protected $usage = 'avegacms:enum [arguments] [options]';
+
+    /**
+     * The Command's Arguments
+     *
+     * @var array
+     */
+    protected $arguments = [
+        'name' => 'The Enum class name.',
+    ];
+
+    /**
+     * The Command's Options
+     *
+     * @var array
+     */
+    protected $options = [
+        '--namespace' => 'Set root namespace. Default: "APP_NAMESPACE".',
+        '--suffix'    => 'Append the component title to the class name (e.g. User => UserModel).',
+        '--force'     => 'Force overwrite existing file.',
+    ];
+
+    /**
+     * Actually execute a command.
+     *
+     * @param array $params
+     */
+    public function run(array $params): void
+    {
+        $this->component = 'Enum';
+        $this->directory = 'Enums';
+        $this->template  = 'avegacmsenum.tpl.php';
+
+        $this->generateClass($params);
+    }
+}
--- src/Config/AvegaCmsGenerators.php
+++ src/Config/AvegaCmsGenerators.php
@@ -11,5 +11,6 @@ class AvegaCmsGenerators
         'avegacms:factory'    => 'AvegaCms\Commands\Generators\Views\avegacmsfactory.tpl.php',
         'avegacms:model'      => 'AvegaCms\Commands\Generators\Views\avegacmsmodel.tpl.php',
         'avegacms:migration'  => 'AvegaCms\Commands\Generators\Views\avegacmsmigration.tpl.php',
+        'avegacms:enum'  => 'AvegaCms\Commands\Generators\Views\avegacmsenum.tpl.php',
     ];
 }
