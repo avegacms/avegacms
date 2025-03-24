9c1521f4f91b059bed50652040b14ad20f552e91
marshmallowfox <shudaof@gmail.com>
2025-03-24 17:28:25 +0600

    поменял на нужное заказчику

--- src/Database/Seeds/AvegaCmsInstallSeeder.php
+++ src/Database/Seeds/AvegaCmsInstallSeeder.php
@@ -734,9 +734,9 @@ class AvegaCmsInstallSeeder extends Seeder
                 'slug'        => 'ru',
                 'locale'      => 'ru_RU',
                 'locale_name' => 'Русская версия',
-                'home'        => 'Главная',
+                'home'        => 'РУГРЕЙН',
                 'extra'       => [
-                    'app_name' => 'Мой новый проект',
+                    'app_name' => 'Аграрный маркетплейс',
                     'og:image' => 'ogDefaultRu.jpg',
                 ],
                 'is_default'    => true,
